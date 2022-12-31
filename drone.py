b8b8b9775cee2900e1fccd4900b4f972f5bb954d
JaPeterson1 <james.peterson.ks@gmail.com>
2022-12-31 14:19:33 -0600

    Added cooling thread that avoids thermal shutoff

--- drone.py
+++ drone.py
@@ -53,11 +53,12 @@ def videoThread():
             for code in decoded:
                 print(code.data.decode('utf-8'))
                 polygon = np.array([list(code.polygon[0]), list(code.polygon[1]), list(code.polygon[2]), list(code.polygon[3])], np.int32).reshape((-1, 1, 2))
-                center = (int((code.polygon[0].x+code.polygon[1].x+code.polygon[2].x+code.polygon[3].x)/4)-img.shape[1]/2,int((code.polygon[0].y+code.polygon[1].y+code.polygon[2].y+code.polygon[3].y)/4)-img.shape[0]/2)
+                center = (int((code.polygon[0].x+code.polygon[1].x+code.polygon[2].x+code.polygon[3].x)/4),int((code.polygon[0].y+code.polygon[1].y+code.polygon[2].y+code.polygon[3].y)/4))
+                print(center)
                 if(hover):
                     currentCodeCenter = center
                     #tl_flight.rc(hoverPIDX(center[0]), hoverPIDY(center[1]))
-                    print(f"RC X:{hoverPIDX(center[0])}, Y{hoverPIDY(center[1])}")
+                    print(f"RC X:{hoverPIDX(center[0]-img.shape[1]/2)}, Y{hoverPIDY(center[1]-img.shape[0]/2)}")
                 cv.circle(img, center, 5, (0, 0, 255), 3)
                 cv.polylines(img, [polygon], True, (0,255,0), 2)
             cv.imshow("img", img)
@@ -68,6 +69,27 @@ def videoThread():
 
 def setHover(on:bool):
     hover=on
+
+def cooling(on:bool):
+    if(on):
+        tl_flight.motor_on().wait_for_completed()
+    else:
+        tl_flight.motor_off().wait_for_completed()
+
+def coolingThread():
+    coolingRunning=False
+    while True:
+        try:
+            droneTemp = tl_drone.get_temp()['temph']
+            if(droneTemp>80 and not coolingRunning):
+                coolingRunning=True
+                cooling(True)
+            elif droneTemp<=80 and coolingRunning:
+                coolingRunning=False
+                cooling(False)
+            time.sleep(1)
+        except Exception as e:
+            print(e)
 
 def close_connection():
     tl_drone.close()
--- main.py
+++ main.py
@@ -2,8 +2,11 @@ import drone
 import threading
 import time
 
-t = threading.Thread(target=drone.videoThread)
-t.start()
+videoThread = threading.Thread(target=drone.videoThread)
+#drone.cooling(True)
+videoThread.start()
+coolingThread = threading.Thread(target=drone.coolingThread)
+coolingThread.start()
 drone.setHover(True)
 time.sleep(50)
 drone.close_connection()
