469f0a91dec7b9406df1714af151e29c110ad145
JaPeterson1 <james.peterson.ks@gmail.com>
2022-12-31 13:48:34 -0600

    Added preliminary hover system.

--- drone.py
+++ drone.py
@@ -7,11 +7,17 @@ import pyzbar.pyzbar as pyzbar
 import cv2 as cv
 import threading
 from robomaster import media
+from simple_pid import PID
 
 tl_drone = robot.Drone()
 tl_drone.initialize()
 tl_flight = tl_drone.flight
 tl_camera = tl_drone.camera
+currentCodeCenter = (0,0)
+hover = False
+hoverPIDX = PID(0, 0, 0, setpoint=0)
+hoverPIDY = PID(0, 0, 0, setpoint=0)
+
 print(f"Finished inializing drone with SDK version {tl_drone.get_sdk_version()}; battery is at {tl_drone.battery.get_battery()}%")
 
 def move(x:int, y:int, z:int):
@@ -47,7 +53,11 @@ def videoThread():
             for code in decoded:
                 print(code.data.decode('utf-8'))
                 polygon = np.array([list(code.polygon[0]), list(code.polygon[1]), list(code.polygon[2]), list(code.polygon[3])], np.int32).reshape((-1, 1, 2))
-                center = (int((code.polygon[0].x+code.polygon[1].x+code.polygon[2].x+code.polygon[3].x)/4),int((code.polygon[0].y+code.polygon[1].y+code.polygon[2].y+code.polygon[3].y)/4))
+                center = (int((code.polygon[0].x+code.polygon[1].x+code.polygon[2].x+code.polygon[3].x)/4)-img.shape[1]/2,int((code.polygon[0].y+code.polygon[1].y+code.polygon[2].y+code.polygon[3].y)/4)-img.shape[0]/2)
+                if(hover):
+                    currentCodeCenter = center
+                    #tl_flight.rc(hoverPIDX(center[0]), hoverPIDY(center[1]))
+                    print(f"RC X:{hoverPIDX(center[0])}, Y{hoverPIDY(center[1])}")
                 cv.circle(img, center, 5, (0, 0, 255), 3)
                 cv.polylines(img, [polygon], True, (0,255,0), 2)
             cv.imshow("img", img)
@@ -55,6 +65,9 @@ def videoThread():
         except Exception as e:
             print(e)
         img = tl_camera.read_video_frame(strategy="newest")
+
+def setHover(on:bool):
+    hover=on
 
 def close_connection():
     tl_drone.close()
--- main.py
+++ main.py
@@ -4,5 +4,6 @@ import time
 
 t = threading.Thread(target=drone.videoThread)
 t.start()
+drone.setHover(True)
 time.sleep(50)
 drone.close_connection()
